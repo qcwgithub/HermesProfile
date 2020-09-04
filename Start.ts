ff8887f64f91b28d1a078aacc6b3fa558052158a
qiucw <qiucw@g-bits.com>
2020-09-04 14:49:22 +0800

    ccc

---
+++ Start.ts
@@ -0,0 +1,36 @@
+import * as express from "express";
+import createHttpError = require("http-errors");
+var path = require('path');
+const bodyParser = require('body-parser');
+
+let app = this.webData.app = express();
+
+app.get('/', (req, res, next) => {
+    console.info(req.body.userID);
+});
+
+//app.use('/', indexRouter);
+//app.use('/users', usersRouter);
+app.use(bodyParser.json());
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createHttpError(404));
+});
+
+// error handler
+app.use(function(err: any, req: any, res: any, next: any) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+let http = require('http').Server(app);
+
+let port = this.baseScript.myLoc().port;
+http.listen(port, () => {
+    this.logger.info('listening on *: ' + port);
+});
